43e369466843ba6bdc93d4389d96e73c4a05bca9
thsghdud13 <thsgdud25@naver.com>
2024-05-29 21:55:36 +0900

    Feat : 채팅관련 API 구현

--- src/main/java/submeet/backend/repository/MemberChatRepository.java
+++ src/main/java/submeet/backend/repository/MemberChatRepository.java
@@ -5,9 +5,12 @@ import submeet.backend.entity.ChatRoom;
 import submeet.backend.entity.Member;
 import submeet.backend.entity.MemberChat;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberChatRepository extends JpaRepository<MemberChat, Long> {
     public Optional<MemberChat> findByMemberAndChatRoom(Member member, ChatRoom chatRoom);
     public boolean existsByMemberAndChatRoom(Member member, ChatRoom chatRoom);
+    public List<MemberChat> findByChatRoom(ChatRoom chatRoom);
+    public List<MemberChat> findByMember(Member member);
 }
